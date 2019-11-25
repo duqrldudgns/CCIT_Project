4bec3ea6cc3dadf2936f10a5cf7e9c0dc2e17bce
root <duqrldudgns@gmail.com>
2019-11-25 15:23:53 +0900

    mac_addr_check

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@
 static MYSQL *conn;                     //database connect
 static MYSQL_RES *res;                  //data save
 static MYSQL_ROW row;                   //data arr sort
-static MYSQL_FIELD *fields;
-static u_int num_fields;
+static MYSQL_FIELD *fields;             //like arr
+static u_int num_fields;                //fields count
 
 void errorMsg(const char *errMsg){
     printf("lately Error Meassage : %s \n", mysql_error(conn));
@@ -32,7 +32,7 @@ int connectDB(const char* server,const char* user, const char* password, const c
     return 0;
 }
 
-int runQuery(const char *query){
+int runQuery(const char *query, const char* mac_data){
     if(mysql_query(conn, query)) {
         errorMsg("MySQL Query Excute fail");
         return -1;
@@ -49,37 +49,44 @@ int runQuery(const char *query){
         errorMsg("MysQL fields fail\n");
         return -3;
     }
+    //select field_name
+    num_fields=mysql_num_fields(res);
+    fields = mysql_fetch_fields(res);
+    printf("fields success : %d \n", num_fields);
+    const char *name = "mac_addr";
+    printf("%12s \n", name);
 
-
-    while( (row=mysql_fetch_row(res))!= nullptr){
-        printf("-");
-        for (u_int i=0; i< num_fields; i++) {
-            printf("%12s",row[i]);
+    int cnt = 0;
+    for(int i=0; i < num_fields;i++) {
+        if(!memcmp(fields[i].name, name, 9)) {
+        cnt = i;
+        break;
         }
-        printf("\n");
     }
+
+    while( (row=mysql_fetch_row(res))!= nullptr){         //one line read
+        printf("%12s \n",row[cnt]);
+        if(!memcmp(row[cnt], mac_data, 17)){
+            printf("catch \n");
+
+
+            break;
+        }
+    }
+
+    //show all pick
+    //    num_fields=mysql_num_fields(res);
+    //    while( (row=mysql_fetch_row(res))!= nullptr){
+    //        printf("-");
+    //        for (u_int i=0; i< num_fields; i++) {
+    //            printf("%12s",row[i]);
+    //        }
+    //        printf("\n");
+    //    }
 
     return 0;
 }
-/*
-void fetchfieldbyID(int id, char *buffer, u_int len){
-    strncpy(buffer, row[id], len);
-}
 
-void fetchfieldbyName(const char *name, char *buffer, u_int len){       //find and copy
-    num_fields = mysql_num_fields(res);
-    fields = mysql_fetch_fields(res);
-
-    for(u_int i=0; i < num_fields; i++) {
-        if(!strcmp(fields[i].name, name)) {
-            if(row[i]) {
-                strncpy(buffer, row[i], len);
-                return;
-            }
-        }
-    }
-}
-*/
 void closeDB(){
     if(res) mysql_free_result(res);
     mysql_close(conn);
@@ -89,15 +96,19 @@ int main() {
     const char *server = "10.100.111.52";
     const char *db_id = "dbadmin";
     const char *db_pw = "CCITdudgns23!@";
-    const char *database = "custom";        // DB name
-    const char *query = "select * from info";
+    const char *database = "Project";        // DB name
+    const char *select_query = "select * from Device";
+    const char *insert_query = "insert";
+
+    const char *mac_data = "cc:cc:cc:cc:cc:cc";
+    const char *time_data;
 
     if (connectDB(server, db_id, db_pw, database)<0) {
         closeDB();
         return 0;
     }
 
-    if (runQuery(query) < 0) {
+    if (runQuery(select_query, mac_data) < 0) {
         closeDB();
         return 0;
     }
