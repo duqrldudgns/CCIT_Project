b845c24e2fd1a35f4b83455c55bc177c6c1b27f3
root <duqrldudgns@gmail.com>
2019-12-04 18:14:15 +0900

    web server

---
+++ web/list_static.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<script language='javascript'>
+//  window.setTimeout('window.location.reload()',3000); //3초마다 리플리쉬 시킨다 1000이 1초가 된다.
+ </script>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<style>
+table{border:1px solid gray; border-collapse:collapse;}
+td{border:1px solid gray;padding:5px;}
+</style>
+</head>
+<body>
+<?php
+
+$conn = mysqli_connect("localhost", "root", "ccit2019","Project");
+mysqli_query('SET NAMES utf8');
+if (!$conn) {
+echo "Unable to connect to DB: " . mysqli_error();
+exit;
+}
+
+$sql = "select *
+from Log
+where time >= '2019-12-01 17:00:00'
+and time <= '2019-12-30 17:30:00'";
+
+$result = mysqli_query($conn,$sql);
+
+if (!$result) {
+echo "Could not successfully run query ($sql) from DB: " . mysqli_error();
+exit;
+}
+
+if (mysqli_num_rows($result) == 0) {
+echo "No rows found, nothing to print so am exiting";
+exit;
+}
+
+// While a row of data exists, put that row in $row as an associative array
+// Note: If you're expecting just one row, no need to use a loop
+// Note: If you put extract($row); inside the following loop, you'll
+// then create $userid, $fullname, and $userstatus
+
+echo "<table>";
+echo "<tr><td>num</td><td>mac_addr</td><td>pwr</td><td>time</td></tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+echo "<tr><td>{$row['num']}</td><td>{$row['mac_addr']}</td><td>{$row['pwr']}</td><td>{$row['time']}</td></tr>";
+}
+echo "</table>";
+mysqli_free_result($result);
+
+?>
+</body>
+</html> 
